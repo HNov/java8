ef11a097c536aa8777df90833e523e95408030a1
wenbo <1260568917@qq.com>
2018-08-22 21:15:07 +0800

    update

--- console/src/main/java/com/nov/console/Mian.java
+++ console/src/main/java/com/nov/console/Mian.java
@@ -2,7 +2,10 @@ package com.nov.console;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 public class Mian {
@@ -11,6 +14,8 @@ public class Mian {
 
     public static void main(String[] args) {
         List();
+        hashMap();
+        concurrentHashMap();
         System.out.println("Hello Word!");
 
     }
@@ -31,9 +36,59 @@ public class Mian {
         list1.add("2");
         list1.add("3");
 
+
         list2 = list1.stream().filter(s -> s != "1").collect(Collectors.toList());
         System.out.println(list2.toString());
     }
 
+
+    public static void hashMap() {
+        Map<String, Integer> map = new HashMap<String, Integer>(32, 0.75f);
+
+        map.put("悟空", 30);
+        map.put("悟空", 31);
+        int value = map.get("悟空");
+        System.out.println(value);
+        for (int i = 1; i < 100; i++) {
+            map.put("key" + i, i);
+        }
+        int length = 32;
+        long start1 = System.currentTimeMillis();
+        for (int i = 1; i < 1000; i++) {
+
+            //求余数
+            int model = i & (length - 1);
+            System.out.println(" h & (length - 1)=" + model);
+        }
+        long start2 = System.currentTimeMillis();
+
+        for (int i = 1; i < 1000; i++) {
+            int model = i % length;
+            System.out.println(" h % length=" + model);
+        }
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("& 耗时：" + (start2 - start1));
+        System.out.println("% 耗时：" + (endTime - start2));
+        System.out.println("时差：" + ((endTime - start2) - (start2 - start1)));
+    }
+
+
+    public static void string() {
+        String str = "";
+
+        str.hashCode();
+    }
+
+    public static void concurrentHashMap() {
+
+        ConcurrentHashMap concurrentHashMap = new ConcurrentHashMap();
+        for (int i = 1; i < 1000; i++) {
+            concurrentHashMap.put(i, i);
+        }
+        for (int i = 1; i < 1000; i++) {
+            System.out.println("【" + i + "】 Value：" + concurrentHashMap.get(i));
+        }
+    }
 }
 
